3e5b93f38f557aac01d1359fb1af7f9e363338a5
Eragon580101 <erewhonaden23@gmail.com>
2024-06-11 10:15:01 +0545

    Update: Add multiple instance of caurosel

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import Caurosel from "./Components/Image_Caurosel";
 import "./style.css";
 
-const app = document.getElementById("app");
-if (app) {
-  let images: string[] = [];
-  for (let i = 0; i < 10; i++) {
-    images.push(`image-${i + 1}.jpg`);
-  }
+let images: string[] = [];
+for (let i = 0; i < 10; i++) {
+  images.push(`image-${i + 1}.jpg`);
+}
 
+const app = document.getElementById("app");
+const flexCaurosel = document.querySelector<HTMLDivElement>(
+  ".image-caurosel-flex"
+);
+if (app) {
   const image_carousel_1 = new Caurosel("caurosel-1", images, 1200, 600, 2000);
   image_carousel_1.appendTo(app);
+}
+
+if (flexCaurosel) {
   const image_carousel_2 = new Caurosel("caurosel-1", images, 600, 300, 1000);
-  image_carousel_2.appendTo(app);
-  const image_carousel_3 = new Caurosel("caurosel-1", images, 800, 500, 500);
-  image_carousel_3.appendTo(app);
+  image_carousel_2.appendTo(flexCaurosel);
+  const image_carousel_3 = new Caurosel("caurosel-1", images, 600, 300, 500);
+  image_carousel_3.appendTo(flexCaurosel);
 }
